069953290cb831af2fd330da96266691d832742f
SenyorTerry <37777549+SenyorTerry@users.noreply.github.com>
2019-03-15 16:16:51 +0100

    Update main.cpp
    "Utilizamos la inversa en vez de una pila"

--- Practica 4 MPA S1/main.cpp
+++ Practica 4 MPA S1/main.cpp
@@ -1,29 +1,43 @@
+/*
+Juan Francisco Torres Martínez - alu80
+Alejandro Gallego López - alu37
+Santiago Moltó Martínez - alu55
+*/
+
 #include <iostream>
 #include <stack>
 
 using namespace std;
 
+int programa1(int x, int y){
+
+    int x0 = x;
+    int y0 = y;
+    int s = 0;
+
+    while(x0 > 1){
+        //pila.push(-x+y);
+        x0 = x0 - 1;
+        y0 = y0 + 2;
+    }
+    s = y0;
+    while(y0 != y){
+        y0 = y0 - 2;
+        x0 = x0 + 1;
+        s = s - x0 + y0;
+    }
+    return s;
+}
+
 int main()
 {
-    stack <int> pila;
-    int x = 5;
-    int y = 4;
-
+    int x;
+    int y;
     cout << "Introduce valores de x:";
     cin >> x;
     cout << "\nIntroduce valores de y:";
     cin >> y;
 
-    while(x > 1){
-        pila.push(-x+y);
-        x = x - 1;
-        y = y + 2;
-    }
-
-    while(! pila.empty()){
-        y = y + pila.top();
-        pila.pop();
-    }
-
-    cout << "\nResultado: " << y << endl;
+    cout << "\nResultado: " << programa1(x,y) << endl;
 }
+
